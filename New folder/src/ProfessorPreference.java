3c4589a865e2e7cd8f0be5a7e753af8215ca6ffa
Jake Maranzatto <maranzatto1@yahoo.com>
2018-02-27 21:57:33 -0500

    Small update
    added helper functions for our data.  starting to work on algorithm

--- New folder/src/ProfessorPreference.java
+++ New folder/src/ProfessorPreference.java
@@ -1,8 +1,41 @@
+import java.util.ArrayList;
 
 public class ProfessorPreference {
-	int[] coursePreferenceIDs;			//all three of these should line up.  so 
-	int[] roomPreferenceIDs;			//the indices of each relate to a single
-	CourseTimes[] coursePreferences;	//preference
+	ClassInfo[] preferences;
 	
+	ProfessorPreference(ClassInfo[] p){
+		this.preferences = p;
+	}
 	
+	ClassInfo[] getPreferences() {	
+		//returns all preferences
+		return this.preferences;
+	}
+	
+	ClassInfo getPreference(int index) {
+		//returns the specific preference
+		return this.preferences[index];
+	}
+	
+	Integer[] getClasses() {
+		//returns all unique classes the professor will like to teach
+		ArrayList<Integer> uniqueClasses = new ArrayList<Integer>();
+		for(ClassInfo c : preferences) {
+			if (uniqueClasses.contains(c.getCourseID()) == false){
+				uniqueClasses.add(c.getCourseID());
+			}
+		}
+		return uniqueClasses.toArray(new Integer[0]);
+	}
+	
+	ClassInfo[] getSingleClassPreference(int cID) {
+		ArrayList<ClassInfo> singlePreference = new ArrayList<ClassInfo>();
+		for(ClassInfo c : preferences) {
+			if(c.getCourseID() == cID) {
+				singlePreference.add(c);
+			}
+		}
+		return singlePreference.toArray(new ClassInfo[0]);
+		
+	}
 }
